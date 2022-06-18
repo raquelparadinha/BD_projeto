1e78386a276f64257e13ce2068bb0a94ebbe06dc
Raquel Paradinha <75377292+raquelparadinha@users.noreply.github.com>
2022-06-18 02:26:14 +0100

    mais

--- SQL/function.sql
+++ SQL/function.sql
@@ -1,4 +1,8 @@
+drop function Mercado.obterFuncionarios
+go
 create function Mercado.obterFuncionarios() returns table 
 as 
 	return (select * from funcionarios)
 go
+
+select * from Mercado.obterFuncionarios()
