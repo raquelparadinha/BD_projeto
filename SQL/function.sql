845330d10be1057d6a7881fd39ef27ae2b85401e
Raquel Paradinha <75377292+raquelparadinha@users.noreply.github.com>
2022-06-20 23:46:25 +0100

    fiz nada

--- SQL/function.sql
+++ SQL/function.sql
@@ -1,3 +1,6 @@
+use p8g2
+
+/* OBTER A LISTA DE FUNCIONÁRIOS */
 drop function Mercado.obterFuncionarios
 go
 create function Mercado.obterFuncionarios() returns table 
@@ -5,6 +8,7 @@ as
 	return (select * from Mercado.Funcionario)
 go
 
+/* OBTER A LISTA DE CHEFES DE LOJA */
 drop function Mercado.obterChefeLoja
 go
 create function Mercado.obterChefeLoja() returns table
@@ -14,6 +18,8 @@ as
 			select * from Mercado.Chefe_Loja_antigo)
 go
 
+
+/* OBTER A LISTA DE RESPONSÁVEIS */
 drop function Mercado.obterRespOp
 go
 create function Mercado.obterRespOp() returns table
@@ -23,6 +29,7 @@ as
 			select * from Mercado.Resp_Op_antigo)
 go
 
+/* OBTER A LISTA DE OP CAIXA */
 drop function Mercado.obterOpCaixa
 go
 create function Mercado.obterOpCaixa() returns table
@@ -32,6 +39,7 @@ as
 			select * from Mercado.Op_Caixa_antigo)
 go
 
+/* OBTER A LISTA DE ATEND CLIENTES */
 drop function Mercado.obterAtendCliente
 go
 create function Mercado.obterAtendCliente() returns table
@@ -41,6 +49,7 @@ as
 			select * from Mercado.Atend_Cliente_antigo)
 go
 
+/* OBTER A LISTA DE REPOSITORES */
 drop function Mercado.obterReposicao
 go
 create function Mercado.obterReposicao() returns table
@@ -50,6 +59,7 @@ as
 			select * from Mercado.Reposicao_antigo)
 go
 
+/* OBTER A LISTA DE CAIXAS */
 drop function Mercado.obterCaixas
 go
 create function Mercado.obterCaixas() returns table
@@ -57,6 +67,7 @@ as
 	return (select * from Mercado.Caixa)
 go
 
+/* OBTER A LISTA DE BALCÕES */
 drop function Mercado.obterBalcao
 go
 create function Mercado.obterBalcao() returns table
@@ -64,6 +75,7 @@ as
 	return (select * from Mercado.Balcao)
 go
 
+/* OBTER A LISTA DE ARMAZÉNS */
 drop function Mercado.obterArmazem
 go
 create function Mercado.obterArmazem() returns table
@@ -71,6 +83,7 @@ as
 	return (select * from Mercado.Armazem)
 go
 
+/* OBTER A LISTA DE SECÇÕES */
 drop function Mercado.obterSeccao
 go
 create function Mercado.obterSeccao() returns table
@@ -78,6 +91,7 @@ as
 	return (select * from Mercado.Seccao)
 go
 
+/* OBTER A LISTA DE PRODUTOS */
 drop function Mercado.obterProdutos
 go
 create function Mercado.obterProdutos() returns table
@@ -85,6 +99,7 @@ as
 	return (select * from Mercado.Produto)
 go
 
+/* OBTER A LISTA DE FUNCIONÁRIOS QUE REPÕE CADA SECÇÃO */
 drop function Mercado.obterRepoe
 go
 create function Mercado.obterRepoe() returns table
@@ -93,6 +108,7 @@ as
 go
 
 
+/* VERIFICAR QUAL FOI O ÚLTIMO ID ADICIONADO */
 drop function Mercado.maxID
 go
 create function Mercado.maxID() returns int
